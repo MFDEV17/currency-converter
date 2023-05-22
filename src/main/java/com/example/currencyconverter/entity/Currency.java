533c5db8dd39724c3397140fa07ab5d4a7e83b8b
mfdev17 <maximsushkin02@gmail.com>
2023-05-22 17:46:48 +0300

    added group unique constraint and dynamic update annotation for currency entity

--- src/main/java/com/example/currencyconverter/entity/Currency.java
+++ src/main/java/com/example/currencyconverter/entity/Currency.java
@@ -2,26 +2,33 @@ package com.example.currencyconverter.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Index;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.DynamicUpdate;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
 @Entity
+@Table(indexes = {
+        @Index(columnList = "code, name", unique = true)
+})
+@DynamicUpdate
 public class Currency extends BaseEntity {
 
-  @Column(unique = true, nullable = false)
+  @Column(nullable = false)
   @Size(min = 3, max = 3)
   private String code;
 
-  @Column(unique = true, nullable = false)
+  @Column(nullable = false)
   private String name;
 
-  @Column(unique = true, nullable = false)
+  @Column(nullable = false)
   private String sign;
 }
